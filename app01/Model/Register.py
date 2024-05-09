9602d3e5eabdd10b013d5a020897f97d06036aa3
rocate1 <112960490+rocate1@users.noreply.github.com>
2024-05-09 15:01:13 +0800

    Create Register.py

---
+++ app01/Model/Register.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify, request
+from Dao.User import User
+from app import db
+register_blueprint = Blueprint('register', __name__)
+@register_blueprint.route("/register", methods=["POST"])
+def user_register():
+    """
+    用户注册
+    :return:
+    """
+    data = request.get_json()
+    nickname = data.get("nickname")
+    phoneNumber = data.get("phoneNumber")
+    password = data.get("password")
+    # 检查手机号是否已经被注册
+    existing_user = User.query.filter_by(PhoneNumber=phoneNumber).first()
+    if existing_user:
+        return jsonify({
+            "status": "Error",
+            "message": "手机号已经被注册"
+        }), 400
+    # 创建新用户并保存到数据库
+    new_user = User(Username=nickname, PhoneNumber=phoneNumber, Password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({
+        "status": "Success",
+        "message": "注册成功"
+    })
