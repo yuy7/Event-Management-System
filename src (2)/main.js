919e5a785ad0dd6e0cceea9aefdf834d4ff05df8
xiaochenn <1173878818@qq.com>
2024-05-15 14:30:17 +0800

    event create & manage 实现

--- src (2)/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router.js'
-import mitt from 'mitt'
- 
-const app=createApp(App)
-app.config.globalProperties.emitter = mitt()
-app.use(router)
-app.mount('#app')
