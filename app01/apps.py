bbedd85f52f97b866cd63558b10df5ef1d8609f2
ljy <linjingyi3317@163.com>
2024-05-16 15:28:23 +0800

    主页

--- app01/apps.py
+++ app01/apps.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
 # username: root, password: root
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/event?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/emsdb?charset=utf8'
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
