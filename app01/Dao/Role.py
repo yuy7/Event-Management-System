c5cf44dc2068a52c7759fd9e25e4e8e74de1664f
xiaochenn <1173878818@qq.com>
2024-05-20 16:19:08 +0800

    身份申请，个人界面

---
+++ app01/Dao/Role.py
@@ -0,0 +1,6 @@
+from __init__ import db
+
+class Role(db.Model):
+    __tablename__ = 'Role'
+    roleID = db.Column('roleID', db.Integer, primary_key=True)
+    roleName = db.Column('roleName', db.String(45))
---
+++ app01/Dao/RoleApply.py
@@ -0,0 +1,7 @@
+from __init__ import db
+
+class RoleApply(db.Model):
+    __tablename__ = 'RoleApply'
+    roleApplyID = db.Column('roleApplyID', db.Integer, primary_key=True)
+    userID = db.Column('userID', db.Integer,db.ForeignKey('User.UserID'))
+    roleID = db.Column('roleID', db.Integer,db.ForeignKey('Role.roleID'))
