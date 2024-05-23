4cf26fd74b1dfd2c0a69182cd89c74b4e7e5d183
xiaochenn <1173878818@qq.com>
2024-05-23 23:14:10 +0800

    超管审核身份

---
+++ app01/Model/RoleApplyCheck.py
@@ -0,0 +1,36 @@
+from flask import jsonify, request, session
+from Dao.User import User
+from Dao.RoleApply import RoleApply
+from Dao.Role import Role
+from __init__ import db
+
+# 显示所有身份申请，GET方法
+def getRoleApply():
+    roleApplyList = db.session.query(RoleApply, Role.roleName).join(Role, RoleApply.roleID == Role.roleID).filter(RoleApply.roleState == 0).all()
+    res = []
+    for roleApply,roleName in roleApplyList:
+        res.append({
+            "roleApplyID": roleApply.roleApplyID,
+            "userID": roleApply.userID,
+            "roleName": roleName,
+        })
+    return jsonify(res)
+
+
+
+# 接受身份申请，POST方法
+def acceptRoleApply():
+    roleApplyID = request.json.get("roleApplyID")
+    roleApply = RoleApply.query.filter_by(roleApplyID=roleApplyID).first()
+    roleApply.roleState = 1
+    try:
+        db.session.commit()
+        return jsonify({
+            "status": "Success"
+        })
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({
+            "status": "Error",
+            "message": str(e)
+        }), 500
