564f43bdb178b52a87f57338bb9fc29375a4f5e7
xiaochenn <1173878818@qq.com>
2024-05-27 20:49:12 +0800

    活动创建后端 & 数据库时间转换表

--- app01/Model/EventCreate.py
+++ app01/Model/EventCreate.py
@@ -3,17 +3,29 @@ from Dao.Event import Event
 from __init__ import db
 
 def EventCreate():
+    # user_id = session.get("userID")
+    user_id = 251101164
     data = request.get_json()
     eventName = data.get("name")
-    eventStartDate = data.get("startDate")
-    eventEndDate = data.get("endDate")
-    eventLocation = data.get("location")
-    requireApproval = data.get("requireApproval", False)  # 默认为False，如果请求中没有提供，则使用False
-
-    user_id = session.get("userID")
+    date = data.get("date")
+    eventTypeID = data.get("eventTypeID")
+    numberOfPeople = data.get("numberOfPeople")
+    preferredLocation = data.get("preferredLocation")
+    requireApproval = data.get("requireApproval")
+    time = data.get("time")
+    
     
     # 创建新活动并保存到数据库
-    newEvent = Event(eventName=eventName, eventStartDate=eventStartDate, eventEndDate=eventEndDate, eventLocation=eventLocation, requireApproval=requireApproval, reservationUserId=user_id)
+    newEvent = Event(
+        eventName=eventName,
+        date=date,
+        reservationUserId=user_id,
+        eventTypeID=eventTypeID,
+        numberOfPeople=numberOfPeople,
+        preferredLocation=preferredLocation,
+        requireApproval=requireApproval,
+        time=time
+    )
     db.session.add(newEvent)
     db.session.commit()
     return jsonify({
