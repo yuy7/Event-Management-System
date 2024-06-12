c3ab265461562aa9cdbdda305ac9e1f321a7eccd
yy <2511011639@qq.com>
2024-06-12 19:36:38 +0800

    update user add event

--- app01/Dao/UserAddEvent.py
+++ app01/Dao/UserAddEvent.py
@@ -2,6 +2,7 @@ from __init__ import db
 
 class UserAddEvent(db.Model):
     __tablename__ = 'user_add_event'
-    userID = db.Column(db.Integer, db.ForeignKey('User.UserID'), primary_key=True)
-    eventID = db.Column(db.Integer, db.ForeignKey('event.eventID',primary_key=True))
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey('User.UserID'))
+    eventID = db.Column(db.Integer, db.ForeignKey('event.eventID'))
     state = db.Column(db.Integer, default=0)  # 默认值为0表示“审批中”, 1表示“已加入”, 2表示“被拒绝”
